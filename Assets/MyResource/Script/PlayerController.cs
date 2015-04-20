60f21a5085aa2341f3e3956defe1a57951de0769
Shu Sugy <shew.curn@gmail.com>
2015-04-20 16:48:21 +0900

    アタックアニメーションを再生できるようになるまで

--- Assets/MyResource/Script/PlayerController.cs
+++ Assets/MyResource/Script/PlayerController.cs
@@ -18,10 +18,12 @@ public class PlayerController : MonoBehaviour {
 	//ここから攻撃用設定
 	//アクションを起こす対象を格納する
 	public GameObject TargetObject;
-
+	public bool action_doing;
 
 	private bool jumping;
 
+	private float v;
+	private float h;
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
@@ -29,35 +31,46 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//hに縦方向の入力を,vに横方向の入力を取得する
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
+
+		if (!action_doing) {
+			//hに縦方向の入力を,vに横方向の入力を取得する
+			h = Input.GetAxisRaw ("Horizontal");
+			v = Input.GetAxisRaw ("Vertical");
+
+			Vector3 direction = transform.TransformDirection (new Vector3 (0, 0, v).normalized);
+			if (Input.GetButton ("Jump")) {
+				StartCoroutine (jump ());
+			}
+
+			if (Input.GetButton ("Fire1")) {
+				if (TargetObject == null){
+					//Debug.Log ("Target is not set.");
+				} else {
+					StartCoroutine (attack ());
+				}
+			}
+
+			//キャラクターを回転させる
+			transform.Rotate (0, h * rotateSpeed, 0);
+
+			//vの入力値をMecanimに反映
+			if (!jumping) {
+				animator.SetFloat ("speed", v);		//directionと移動速度を元にキャラクターの移動量を計算する
+			}
+
+			//前進の場合
+			if (v > 0) {
+				GetComponent<Rigidbody> ().AddForce (direction * moveSpeed);	
+			}
 		
-		Vector3 direction = transform.TransformDirection(new Vector3 (0,0,v).normalized);
+			//後退の場合
+			if (v < 0) {
+				GetComponent<Rigidbody> ().AddForce (direction * backSpeed);
+			}
 
-		if (Input.GetButton ("Jump")) {
-			StartCoroutine(jump ());
-		}
-		//キャラクターを回転させる
-		transform.Rotate(0, h * rotateSpeed, 0);
-
-		//vの入力値をMecanimに反映
-		if (!jumping){
-			animator.SetFloat ("speed", v);		//directionと移動速度を元にキャラクターの移動量を計算する
-		}
-
-		//前進の場合
-		if (v > 0) {
-			GetComponent<Rigidbody> ().AddForce (direction * moveSpeed);	
-		}
-		
-		//後退の場合
-		if (v < 0) {
-			GetComponent<Rigidbody> ().AddForce (direction * backSpeed);
-		}
-
-		if (v == 0) {
-			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			if (v == 0) {
+				GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			}
 		}
 	}
 
@@ -68,6 +81,16 @@ public class PlayerController : MonoBehaviour {
 		yield return new WaitForSeconds (0.70f);
 		animator.SetBool ("jumping", false);
 		jumping = false;
+	}
+
+	IEnumerator attack(){
+		action_doing = true;
+		animator.SetFloat ("speed", 0);
+		GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		animator.SetBool ("attacking", true);
+		yield return new WaitForSeconds (0.70f);
+		animator.SetBool ("attacking", false);
+		action_doing = false;
 	}
 	
 }
